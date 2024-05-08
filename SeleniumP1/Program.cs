56a1f23f3bf5b8f51a663464af38c0bc7cb491d2
Can Aşık <qratipum@gmail.com>
2024-05-08 20:54:31 +0300

    p1.1#

--- SeleniumP1/Program.cs
+++ SeleniumP1/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 
 namespace SeleniumP1
 {
@@ -10,6 +14,31 @@ namespace SeleniumP1
     {
         static void Main(string[] args)
         {
+
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.amazon.com.tr/");
+
+            driver.FindElement(By.Id("twotabsearchtextbox")).SendKeys("zx6r");
+            driver.FindElement(By.Id("nav-search-submit-button")).SendKeys(Keys.Enter);
+
+            Thread.Sleep(2500);
+            driver.FindElement(By.XPath("//*[@id=\"search\"]/div[1]/div[1]/div/span[1]/div[1]/div[6]/div/div/span/div/div/div[2]/div[1]/h2/a")).Click();
+
+            Thread.Sleep(2500);
+            driver.FindElement(By.Id("add-to-cart-button")).Click();
+
+            Thread.Sleep(2500);
+            driver.FindElement(By.XPath("//*[@id=\"sw-gtc\"]/span/a")).Click();
+
+            Thread.Sleep(2500);
+            driver.FindElement(By.XPath("//*[@id=\"sc-buy-box-ptc-button\"]")).Click();
+
+
+
+
+
         }
     }
 }
